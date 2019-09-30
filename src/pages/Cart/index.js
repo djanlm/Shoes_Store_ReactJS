fd31d4e0a6f1979e0dec7f3cbf64f086d2551b53
Djanilson Martins <djanlm@hotmail.com>
2019-09-30 10:41:42 -0300

    added reactotron

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  MdDelete,
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUCT</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+                alt="shoes"
+              />
+            </td>
+            <td>
+              <strong>Very nice shoes</strong>
+              <span>$129,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Complete order</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>$1290,99</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
---
+++ src/util/format.js
@@ -0,0 +1,9 @@
+export const { format: formatPrice } = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+/* export const format = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+}); */
